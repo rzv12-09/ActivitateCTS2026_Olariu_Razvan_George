7a8643b9ac5d5ab52d57b08b288546cf40427aeb
rzv12-09 <olariurazvan20@gmail.com>
2026-02-22 19:02:36 +0200

    Create ZooKeeper class

---
+++ Seminar01/src/ro/ase/cts/seminar01/zoo/ZooKeeper.java
@@ -0,0 +1,14 @@
+package ro.ase.cts.seminar01.zoo;
+
+import ro.ase.cts.seminar01.animale.Animal;
+
+public class ZooKeeper {
+    private String name;
+    private void feedAnimal(Animal animal){
+        animal.eat(this.name);
+    }
+
+    public ZooKeeper(String name) {
+        this.name = name;
+    }
+}
