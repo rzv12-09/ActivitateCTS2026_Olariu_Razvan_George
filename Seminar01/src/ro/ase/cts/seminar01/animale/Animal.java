2b0d27aa19aea8e20bcb1a7dcb47a22660e58504
rzv12-09 <olariurazvan20@gmail.com>
2026-02-22 18:52:16 +0200

    Add constructors in animal classes

--- Seminar01/src/ro/ase/cts/seminar01/animale/Animal.java
+++ Seminar01/src/ro/ase/cts/seminar01/animale/Animal.java
@@ -4,4 +4,9 @@ public abstract class Animal {
     protected String name;
     protected int age;
     public abstract void eat(String ZooKeeperName);
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
--- Seminar01/src/ro/ase/cts/seminar01/animale/Lion.java
+++ Seminar01/src/ro/ase/cts/seminar01/animale/Lion.java
@@ -6,4 +6,9 @@ public class Lion extends Animal{
     public void eat(String ZooKeeperName) {
         System.out.printf("ZooKeeperul %s a hranit Leul %s",ZooKeeperName,super.name);
     }
+
+    public Lion(String name, int age) {
+        super(name, age);
+    }
+
 }
