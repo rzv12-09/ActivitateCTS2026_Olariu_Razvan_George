8e859fdc6b6812d406fe1280956272175ba071df
rzv12-09 <olariurazvan20@gmail.com>
2026-02-26 14:41:56 +0200

    Add AngajatiLoader extending IDataLOader

--- Seminar02/src/ro/ase/cts/classes/Utils.java
+++ Seminar02/src/ro/ase/cts/classes/Utils.java
@@ -14,26 +14,7 @@ public class Utils {
 	}
 
 	public static List<Angajat> readAngajati(String file) throws FileNotFoundException {
-		Scanner input2 = new Scanner(new File(file));
-		input2.useDelimiter(",");
-		List<Angajat> angajati = new ArrayList<Angajat>();
 
-		while (input2.hasNext()) {
-			String nume = input2.next();
-			String prenume = input2.next();
-			int varsta = input2.nextInt();
-			int punctaj = input2.nextInt();
-			int nr = input2.nextInt();
-			String[] vect = new String[5];
-			for (int i = 0; i < nr; i++)
-				vect[i] = input2.next();
-			int salariu = input2.nextInt();
-			String ocupatie = input2.next();
-			Angajat a = new Angajat(nume, prenume, varsta, punctaj, nr, vect, salariu, ocupatie);
-			angajati.add(a);
-		}
-		input2.close();
-		return angajati;
 	}
 
 	public static List<Elev> readPupil(String file) throws FileNotFoundException {
--- Seminar02/src/ro/ase/cts/classes/loaders/AngajatiLoader.java
+++ Seminar02/src/ro/ase/cts/classes/loaders/AngajatiLoader.java
@@ -1,4 +1,37 @@
 package ro.ase.cts.classes.loaders;
 
-public class AngajatiLoader {
+import ro.ase.cts.classes.Angajat;
+import ro.ase.cts.classes.Aplicant;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AngajatiLoader implements IDataLoader {
+
+    @Override
+    public List<Aplicant> load(String file) throws FileNotFoundException {
+        Scanner input2 = new Scanner(new File(file));
+        input2.useDelimiter(",");
+        List<Aplicant> angajati = new ArrayList<Aplicant>();
+
+        while (input2.hasNext()) {
+            String nume = input2.next();
+            String prenume = input2.next();
+            int varsta = input2.nextInt();
+            int punctaj = input2.nextInt();
+            int nr = input2.nextInt();
+            String[] vect = new String[5];
+            for (int i = 0; i < nr; i++)
+                vect[i] = input2.next();
+            int salariu = input2.nextInt();
+            String ocupatie = input2.next();
+            Angajat a = new Angajat(nume, prenume, varsta, punctaj, nr, vect, salariu, ocupatie);
+            angajati.add(a);
+        }
+        input2.close();
+        return angajati;
+    }
 }
